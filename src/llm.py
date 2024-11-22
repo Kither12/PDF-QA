9daa32ad34711d8c29c6e6cc146db737f75b45d8
Kither12 <developerkither2509@gmail.com>
2024-11-22 21:25:07 +0700

    fix import module

--- src/llm.py
+++ src/llm.py
@@ -1,6 +1,6 @@
 import os
 
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain_core.prompts import PromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
